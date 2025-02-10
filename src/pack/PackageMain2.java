ca5a6c37aa9a31856cf2bc87dd87b69713291835
Jmpak99 <jmpak99@gmail.com>
2025-02-10 21:05:42 +0900

    package

--- src/pack/PackageMain2.java
+++ src/pack/PackageMain2.java
@@ -1,10 +1,12 @@
 package pack;
 
-public class PackageMain1 {
+import pack.a.User;
+
+public class PackageMain2 {
 
     public static void main(String[] args) {
         Data data = new Data();
-        pack.a.User user = new pack.a.User();
+        User user = new User();
 
     }
 }
