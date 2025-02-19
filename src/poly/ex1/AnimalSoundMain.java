02aac41c0945bea07ebac9c6abadeaca3734dfa4
Jmpak99 <jmpak99@gmail.com>
2025-02-19 21:09:24 +0900

    poly

--- src/poly/ex1/AnimalSoundMain.java
+++ src/poly/ex1/AnimalSoundMain.java
@@ -15,6 +15,10 @@ public class AnimalSoundMain {
         cat.sound();
         System.out.println("동물 소리 테스트 종료");
 
+        soundCaw(caw);
+    }
+
+    private static void soundCaw(Caw caw) {
         System.out.println("동물 소리 테스트 시작");
         caw.sound();
         System.out.println("동물 소리 테스트 종료");
